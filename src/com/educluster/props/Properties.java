b939260ebcb7848bef988a1182f90ea416aa81f1
Mahen Samaranayake <wxmahen@gmail.com>
2016-11-10 12:19:59 +0530

    Initial Commit

--- src/com/educluster/props/Properties.java
+++ src/com/educluster/props/Properties.java
@@ -2,7 +2,7 @@ package com.educluster.props;
 
 public class Properties {
 
-    public static String MASTER_PKG = "com.radicalz.persistence";
+    public static String MASTER_PKG = "com.educluster.persistence";
     public static String REPO = "repo";
     public static String MODEL = "model";
 }
